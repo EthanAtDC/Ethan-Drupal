98cc0f90a0bdb012e10a3c9b938c0b10154718d7
Ethan Morissette <ethan.morissette@Ethans-MacBook-Pro-2.local>
2022-09-22 14:26:48 -0700

    Little compy of example - Failing to get it working - Blocked by a call to getEntityTypeId - Can't find this call

--- web/modules/ethan/src/Form/EthanForm.php
+++ web/modules/ethan/src/Form/EthanForm.php
@@ -2,85 +2,39 @@
 
 namespace Drupal\ethan\Form;
 
-use Drupal\Core\Form\FormBase;
+use Drupal\Core\Entity\ContentEntityForm;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Language\Language;
 
 /**
  * Provides a ethan form.
  */
-class EthanForm extends FormBase {
+class EthanForm extends ContentEntityForm {
 
-  /**
-   * {@inheritdoc}
-   */
-  public function getFormId() {
-    return 'ethan_ethan';
-  }
 
   /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
 
-    $form['contacts'] = array(
-      '#type' => 'table',
-      '#header' => array( 
-        $this->t('Name'),
-        $this->t('Phone'),
-      ),
-    );
-    for ($i = 1; $i <= 2; $i++) {
-      $form['contacts'][$i]['#attributes'] = array(
-        'class' => array(
-          'someone',
-          'something',
-        ),
-      );
-      $form['contacts'][$i]['name'] = array(
-        '#type' => 'textfield',
-        '#title' => $this->t('Name'),
-        '#title_display' => 'invisible',
-      );
-    }
+    $form = parent::buildForm($form, $form_state);
+    $entity = $this->entity;
 
-    $form['contacts'][]['colspan_example'] = array(
-      '#wrapper_attributes' => array(
-        'colspan' => 1,
-        'class' => array(
-          'someone',
-          'something',
-        ),
-      ),
-
-    );
-
-    $form['actions'] = [
-      '#type' => 'actions',
-    ];
-    $form['actions']['submit'] = [
-      '#type' => 'submit',
-      '#value' => $this->t('Send'),
+    $form['contacts'] = [
+      '#type' => 'language_select',
+      '#defualt_value' => $entity->getUntranslated()->language()->getId(),
+      '#languages' => Language::STATE_ALL,
     ];
 
     return $form;
 
   }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function validateForm(array &$form, FormStateInterface $form_state) {
-    // if (mb_strlen($form_state->getValue('contacts')) < 5) {
-    //   $form_state->setErrorByName('contacts', $this->t('Message should be at least 5 characters.'));
-    // }
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
+  public function save(array $form, FormStateInterface $form_state)
+  {
     $this->messenger()->addStatus($this->t('The form has been sent.'));
-    // $form_state->setRedirect('<front>');
+    $entity = $this->getEntity();
+    $entity->save();
   }
 
 }

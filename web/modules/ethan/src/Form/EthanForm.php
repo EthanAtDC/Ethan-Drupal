38d3c0a1dff7bb367e7bdcab51571213e00cd361
Ethan Morissette <ethan.morissette@Ethans-MacBook-Pro-2.local>
2022-09-22 08:46:18 -0700

    Changed up the form to have a few fields + no redirect

--- web/modules/ethan/src/Form/EthanForm.php
+++ web/modules/ethan/src/Form/EthanForm.php
@@ -22,11 +22,37 @@ class EthanForm extends FormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
 
-    $form['message'] = [
-      '#type' => 'textarea',
-      '#title' => $this->t('Message'),
-      '#required' => TRUE,
-    ];
+    $form['contacts'] = array(
+      '#type' => 'table',
+      '#header' => array( 
+        $this->t('Name'),
+        $this->t('Phone'),
+      ),
+    );
+    for ($i = 1; $i <= 2; $i++) {
+      $form['contacts'][$i]['#attributes'] = array(
+        'class' => array(
+          'someone',
+          'something',
+        ),
+      );
+      $form['contacts'][$i]['name'] = array(
+        '#type' => 'textfield',
+        '#title' => $this->t('Name'),
+        '#title_display' => 'invisible',
+      );
+    }
+
+    $form['contacts'][]['colspan_example'] = array(
+      '#wrapper_attributes' => array(
+        'colspan' => 1,
+        'class' => array(
+          'someone',
+          'something',
+        ),
+      ),
+
+    );
 
     $form['actions'] = [
       '#type' => 'actions',
@@ -37,23 +63,24 @@ class EthanForm extends FormBase {
     ];
 
     return $form;
+
   }
 
   /**
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
-    if (mb_strlen($form_state->getValue('message')) < 10) {
-      $form_state->setErrorByName('message', $this->t('Message should be at least 10 characters.'));
-    }
+    // if (mb_strlen($form_state->getValue('contacts')) < 5) {
+    //   $form_state->setErrorByName('contacts', $this->t('Message should be at least 5 characters.'));
+    // }
   }
 
   /**
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $this->messenger()->addStatus($this->t('The message has been sent.'));
-    $form_state->setRedirect('<front>');
+    $this->messenger()->addStatus($this->t('The form has been sent.'));
+    // $form_state->setRedirect('<front>');
   }
 
 }
---
+++ web/modules/ethan/todo.md
@@ -0,0 +1,16 @@
+# TODO list for project from Matt
+
+
+1. Create simple form to repurpose for my form entity builder
+2. Rework the simple form to create a user
+
+**Need to confirm with Matt that the direction I'm heading with this is correct**
+
+# How I understand an entitiy
+
+- Can basically be anything
+- With this project if I create a form you fill out specfic fields
+- It creates a object of some kind from what you entered
+- Then displays the object at the bottom of the screen
+
+**NOTE maybe use generate to add theming**
